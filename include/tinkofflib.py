7e7cbf656461f33e53ad71729ce3642e266f1eec
HappyManRus <62791404+HappyManRus@users.noreply.github.com>
2022-01-22 10:40:02 +0300

    Update tinkofflib.py

--- include/tinkofflib.py
+++ include/tinkofflib.py
@@ -1,5 +1,6 @@
-from urllib import response
 import grpc
+import uuid
+from include.common_pb2 import Quotation
 import include.stoporders_pb2
 import include.stoporders_pb2_grpc
 import include.orders_pb2
@@ -8,8 +9,10 @@ import include.users_pb2
 import include.users_pb2_grpc
 import include.operations_pb2
 import include.operations_pb2_grpc
-
-# ok
+import include.marketdata_pb2
+import include.marketdata_pb2_grpc
+import include.instruments_pb2
+import include.instruments_pb2_grpc
 
 
 def GetAccountsRequest(connection_data):
@@ -19,8 +22,6 @@ def GetAccountsRequest(connection_data):
     print(response)
     return response
 
-# ok
-
 
 def GetMarginAttributesRequest(connection_data, account_id):
     stub = include.users_pb2_grpc.UsersServiceStub(connection_data['channel'])
@@ -28,8 +29,6 @@ def GetMarginAttributesRequest(connection_data, account_id):
         include.users_pb2.GetMarginAttributesRequest(account_id=account_id), metadata=connection_data['metadata'])
     print(response)
     return response
-
-# ok
 
 
 def GetWithdrawLimitsRequest(connection_data, account_id):
@@ -47,3 +46,49 @@ def GetChannel():
     channel = grpc.secure_channel(
         target='invest-public-api.tinkoff.ru:443', credentials=credentials)
     return channel
+
+def GetPostOrderRequest(connection_data, account_id, figi, quantity, price, direction, order_type):
+    order_id = uuid.uuid4()
+    stub = include.orders_pb2_grpc.OrdersServiceStub(connection_data['channel'])
+    # ВАЖНО!!! переделать множетель, подходит только для акций. Возможно другой для фьючерсов и т.д.
+    price_Quotation = Quotation(units = int(price // 1), nano= int((price - (price // 1)) % 1 * 1000000000))
+    response = stub.PostOrder(include.orders_pb2.PostOrderRequest(account_id=account_id, figi=figi, quantity=quantity, price=price_Quotation, direction=direction, order_type=order_type, order_id = order_id.hex), metadata=connection_data['metadata'])
+    print(response)
+    return response
+
+def GetOrderBookRequest(connection_data, figi, depth = 1):
+    stub = include.marketdata_pb2_grpc.MarketDataServiceStub(connection_data['channel'])
+    response = stub.GetOrderBook(include.marketdata_pb2.GetOrderBookRequest(figi=figi, depth=depth), metadata=connection_data['metadata'])
+    print(response)
+    return response    
+
+def GetTradingStatusRequest(connection_data, figi):
+    stub = include.marketdata_pb2_grpc.MarketDataServiceStub(connection_data['channel'])
+    response = stub.GetTradingStatus(include.marketdata_pb2.GetTradingStatusRequest(figi=figi), metadata=connection_data['metadata'])
+    print(response)
+    return response    
+
+def InstrumentByfigiRequest(connection_data, figi):
+    stub = include.instruments_pb2_grpc.InstrumentsServiceStub(connection_data['channel'])
+    response = stub.GetInstrumentBy(include.instruments_pb2.InstrumentRequest(id_type = 1, id = figi), metadata=connection_data['metadata'])
+    print(response)
+    return response    
+
+def PortfolioRequest(connection_data, account_id):
+    stub = include.operations_pb2_grpc.OperationsServiceStub(connection_data['channel'])
+    response = stub.GetPortfolio(include.operations_pb2.PortfolioRequest(account_id=account_id), metadata=connection_data['metadata'])
+    print(response)
+    return response 
+
+def GetOrdersRequest(connection_data, account_id):
+    stub = include.orders_pb2_grpc.OrdersServiceStub(connection_data['channel'])
+    response = stub.GetOrders(include.orders_pb2.GetOrdersRequest(account_id=account_id), metadata=connection_data['metadata'])
+    print(response)
+    return response     
+
+
+def CancelOrderRequest(connection_data, order_id):
+    stub = include.orders_pb2_grpc.OrdersServiceStub(connection_data['channel'])
+    response = stub.CancelOrder(include.orders_pb2.CancelOrderRequest(order_id=order_id), metadata=connection_data['metadata'])
+    print(response)
+    return response         
